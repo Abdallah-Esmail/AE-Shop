5fa6a1dad8c772943bd6bc1b0c30c663fb3d84f2
Abdallah <abdallahesmail075@gmail.com>
2026-09-05 00:10:41 +0300

    Solve isAuth calling issue in product component

--- src/components/product/Product.jsx
+++ src/components/product/Product.jsx
@@ -14,7 +14,7 @@ import { handleApiError } from "../../utils/handleApiError";
 import { handleApiSuccess } from "../../utils/handleApiSuccess";
 import Spinner from "../spinner/Spinner";
 function Product({ _id, title, quantity, price, imageCover, ratingsAverage }) {
-  const { isAuth } = useSelector((state) => state.auth.isAuth);
+  const { isAuth } = useSelector((state) => state.auth);
   const { data: cart } = useGetMyCartQuery(undefined, { skip: !isAuth });
   const { data: wishlist } = useGetMyWishlistQuery(undefined, {
     skip: !isAuth,
